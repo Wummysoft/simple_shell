4a2dc5013d69904c33fd2bfe2250407c6f132d61
Prince Jetro <adegbuyijephthah@gmail.com>
2023-02-16 12:36:20 -0800

    checking bugs

--- environ.c
+++ environ.c
@@ -3,7 +3,7 @@
 /**
  * _myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
- *	constant function prototype.
+ *          constant function prototype.
  * Return: Always 0
  */
 int _myenv(info_t *info)
@@ -26,10 +26,9 @@ char *_getenv(info_t *info, const char *name)
 
 	while (node)
 	{
-		p = starts_with(node->str, name;
-				if (p && *p)
-
-				return (p);
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
 		node = node->next;
 	}
 	return (NULL);
@@ -37,16 +36,16 @@ char *_getenv(info_t *info, const char *name)
 
 /**
  * _mysetenv - Initialize a new environment variable,
- *	or modify an existing one
+ *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
- *	constant function prototype.
+ *        constant function prototype.
  *  Return: Always 0
  */
 int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		eputs("Incorrect number of arguements\n");
+		_eputs("Incorrect number of arguements\n");
 		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
@@ -57,29 +56,28 @@ int _mysetenv(info_t *info)
 /**
  * _myunsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
- *	constant function prototype.
- * Return: Always 0
+ *        constant function prototype.
+ *  Return: Always 0
  */
 int _myunsetenv(info_t *info)
 {
 	int i;
 
 	if (info->argc == 1)
-
 	{
 		_eputs("Too few arguements.\n");
-
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
 		_unsetenv(info, info->argv[i]);
+
 	return (0);
 }
 
 /**
  * populate_env_list - populates env linked list
  * @info: Structure containing potential arguments. Used to maintain
- *	constant function prototype.
+ *          constant function prototype.
  * Return: Always 0
  */
 int populate_env_list(info_t *info)
@@ -92,7 +90,3 @@ int populate_env_list(info_t *info)
 	info->env = node;
 	return (0);
 }
-
-
-== == == == == == == == == ==
-
